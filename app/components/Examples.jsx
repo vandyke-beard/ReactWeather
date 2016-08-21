88dbdc620514bb05cbe1a61bb89be10530381965
Beard <f210686@gmail.com>
2016-08-21 17:04:00 +0800

    Add grid system & style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,19 +1,19 @@
 var React = require('react');
-
-
-// var Examples = React.createClass({
-//   render : function() {
-//     return (
-//       <h3>Examples Component!</h3>
-//     )
-//   }
-// });
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component!</h3>
-      <p>Welcome to examples page.</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example location to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Taipei">Taipei , Taiwan</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio , Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,25 +1,16 @@
 var React = require('react');
 var Nav = require('Nav');
 
-// var Main = React.createClass({
-//   render: function(){
-//     return (
-//       <div>
-//         <Nav/>
-//         <h2>Main Component</h2>
-//         {this.props.children}
-//       </div>
-//     );
-//   }
-// });
-
 // Stateless Functional Components
 var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main Component</h2>
-      {props.children}
+      <div className="row">
+        <div className="columns midium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 }
